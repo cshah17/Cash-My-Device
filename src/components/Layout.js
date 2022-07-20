b8e328b20c42a861373939ee73199405954a221e
Mohammad <arifawesome7@gmail.com>
2022-07-20 14:55:25 -0700

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -122,7 +122,7 @@ class CustomLayout extends React.Component {
                                         <span className="contect">
                                             1.555.555.5535<br />
                                     7am - 6pm PT<br />
-                                    EVeryday
+                                    EVerYday
                                     </span>
                                         <span className="email">
                                             <Link to='#'>Email Us</Link>
